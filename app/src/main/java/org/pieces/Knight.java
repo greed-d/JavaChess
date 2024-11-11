ec2a605b3dd5a69dc47d565e2f38274b364b50af
greed-d <deekshit.bti@gmail.com>
2024-11-11 16:04:25 +0545

    `add(image)` : add image for chess pieces

---
+++ app/src/main/java/org/pieces/Knight.java
@@ -0,0 +1,10 @@
+package org.pieces;
+
+import org.example.Board;
+
+public class Knight extends Piece {
+  public Knight(Board board, int col, int row, boolean isWhite) {
+    super(board);
+  }
+
+}
