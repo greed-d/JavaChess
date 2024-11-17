44f28bc9b15cdd43138936c4307e76e0cde14d96
greed-d <deekshit.bti@gmail.com>
2024-11-17 14:52:07 +0545

    `add(piece)` : define piece position on the board

--- app/src/main/java/org/example/Board.java
+++ app/src/main/java/org/example/Board.java
@@ -1,29 +1,80 @@
 package org.example;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
+
+import org.pieces.*;
+import org.pieces.Piece;
 
 public class Board extends JPanel {
     public int tileSize = 100;
 
-    int rows = 8;
-    int cols = 8;
+    int row = 8;
+    int col = 8;
+
+    ArrayList<Piece> pieceList = new ArrayList<>();
 
     public Board() {
-        this.setPreferredSize(new Dimension(cols * tileSize, rows * tileSize));
+        this.setPreferredSize(new Dimension(col * tileSize, row * tileSize));
+        addPieces();
         // this.setBackground(Color.green);
+    }
+
+    public void addPieces() {
+        pieceList.add(new Rook(this, 0, 0, false));
+        pieceList.add(new Knight(this, 1, 0, false));
+        pieceList.add(new Bishop(this, 2, 0, false));
+        pieceList.add(new Queen(this, 3, 0, false));
+        pieceList.add(new King(this, 4, 0, false));
+        pieceList.add(new Bishop(this, 5, 0, false));
+        pieceList.add(new Knight(this, 6, 0, false));
+        pieceList.add(new Rook(this, 7, 0, false));
+
+        pieceList.add(new Pawn(this, 0, 1, false));
+        pieceList.add(new Pawn(this, 1, 1, false));
+        pieceList.add(new Pawn(this, 2, 1, false));
+        pieceList.add(new Pawn(this, 3, 1, false));
+        pieceList.add(new Pawn(this, 4, 1, false));
+        pieceList.add(new Pawn(this, 5, 1, false));
+        pieceList.add(new Pawn(this, 6, 1, false));
+        pieceList.add(new Pawn(this, 7, 1, false));
+
+        pieceList.add(new Rook(this, 7, 7, true));
+        pieceList.add(new Knight(this, 6, 7, true));
+        pieceList.add(new Bishop(this, 5, 7, true));
+        pieceList.add(new King(this, 4, 7, true));
+        pieceList.add(new Queen(this, 3, 7, true));
+        pieceList.add(new Bishop(this, 2, 7, true));
+        pieceList.add(new Knight(this, 1, 7, true));
+        pieceList.add(new Rook(this, 0, 7, true));
+        //
+        pieceList.add(new Pawn(this, 0, 6, true));
+        pieceList.add(new Pawn(this, 1, 6, true));
+        pieceList.add(new Pawn(this, 2, 6, true));
+        pieceList.add(new Pawn(this, 3, 6, true));
+        pieceList.add(new Pawn(this, 4, 6, true));
+        pieceList.add(new Pawn(this, 5, 6, true));
+        pieceList.add(new Pawn(this, 6, 6, true));
+        pieceList.add(new Pawn(this, 7, 6, true));
     }
 
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
-        for (int r = 0; r < rows; r++)
-            for (int c = 0; c < cols; c++) {
-                g2d.setColor((c + r) % 2 == 0 ? Color.white : Color.black);
+        for (int r = 0; r < row; r++)
+            for (int c = 0; c < col; c++) {
+                g2d.setColor((c + r) % 2 == 0 ? new Color(188, 192, 204) : new Color(92, 95, 119));
                 g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
 
             }
+
+        for (Piece piece : pieceList) {
+
+            piece.paint(g2d);
+
+        }
     }
 
 }
